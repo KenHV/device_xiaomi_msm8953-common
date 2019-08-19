1d125aa4d1746435f389c846cbe9ed30e37bf27d
KenHV <kenhv@null.net>
2019-08-19 18:13:49 +0530

    prop: Re-enable EIS
    Signed-off-by: KenHV <kenhv@null.net>

--- prop.mk
+++ prop.mk
@@ -52,7 +52,7 @@ persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
 vidc.enc.dcvs.extra-buff-count=2 \
-persist.camera.eis.enable=0 \
+persist.camera.eis.enable=1 \
 persist.camera.HAL3.enabled=1
 
 # Cne/Dpm
